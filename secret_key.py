b75aa862be69c5ef9c2807f49b659d0e2e5f3dde
josephsvk <20kovalik20@gmail.com>
2025-05-11 14:15:52 +0200

    Fix secret key environment variable name and improve output message

--- secret_key.py
+++ secret_key.py
@@ -1,9 +1,9 @@
 import os
 
 # Získanie tajného kľúča z environmentálnej premennej
-secret_key = os.getenv("MY_SECRET_KEY")
+secret_key = os.getenv("SECRET_KEY")
 
 if secret_key:
-    print(f"Tajný kľúč je nastavený")
+    print(f"Tajný kľúč je: {secret_key}")
 else:
     print("Tajný kľúč nebol nastavený! Skontrolujte konfiguráciu.")
